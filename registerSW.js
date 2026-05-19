de1faae4157ecb5b6802e53e6edde54d9b951a19
EduardoPetrini <eduardo.petrini@outlook.com>
2026-05-19 15:48:22 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/french-learning/sw.js', { scope: '/french-learning/' })})}
